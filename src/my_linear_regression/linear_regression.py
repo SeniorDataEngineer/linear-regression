c285c1fb2be198afd155917e6e1b8a13481849d5
JRose <Jameserose8@gmail.com>
2021-01-18 15:00:58 +0000

    Added __init__ to LinearRegression.

--- src/my_linear_regression/linear_regression.py
+++ src/my_linear_regression/linear_regression.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3.8.5
-# Copyright 2020, Rose Software Ltd, All rights reserved.
+# Copyright 2021, Rose Software Ltd, All rights reserved.
 
 # Built-in imports.
 from math import log
@@ -14,4 +14,16 @@ class LinearRegression(HasPropertyMixin):
     classification. It is a supervised machine learning algorithm.
     """
 
-    
+    def __init__(
+            self,
+            eta: float = 0.01,
+            epochs: int = 100,
+            target: str)
+        """
+        Initializes a linear regressor. Takes eta which is the learning
+        rate and epochs which is the number of simulations to run for a
+        given target file path. \n
+        """
+        self.eta = eta
+        self.epochs = epochs
+        self.target = target
