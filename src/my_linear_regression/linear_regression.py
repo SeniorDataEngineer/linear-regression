235cb17de018142ccf345dbad4100b00b725c9e2
JRose <Jameserose8@gmail.com>
2021-01-26 22:13:00 +0000

    Simplified LinearRegressor.

--- src/my_linear_regression/linear_regression.py
+++ src/my_linear_regression/linear_regression.py
@@ -2,12 +2,11 @@
 # Copyright 2021, Rose Software Ltd, All rights reserved.
 
 # Built-in imports.
-from random import random
+from random import randint
 
 # Project imports.
 from .mixin.has_property_mixin import HasPropertyMixin
 from .neuron.artificial_neuron import Neuron
-from .splitter.data_splitter import DataSplitter
 
 # Third-party imports.
 import numpy
@@ -21,11 +20,12 @@ class LinearRegressor(HasPropertyMixin):
 
     def __init__(
             self,
-            eta: float = 0.01,
-            epochs: int = 100,
-            neuron_count: int = 1,
-            threshold: float = 0.0,
-            **kwargs,):
+            samples_train: numpy.ndarray,
+            samples_test: numpy.ndarray,
+            labels_train: numpy.ndarray,
+            labels_test: numpy.ndarray,
+            eta: float = 0.001,
+            epochs: int = 50,):
         """
         Initializes a linear regressor. Takes eta which is the learning
         rate and epochs which is the number of simulations to run for a
@@ -34,122 +34,78 @@ class LinearRegressor(HasPropertyMixin):
         """
         self.eta = eta
         self.epochs = epochs
-        self.threshold = threshold
-        self.solutions = []
-
-        self.splitter = DataSplitter(**kwargs)
-
-        self._init_weights(4, True)
-
-        kwargs = {
-            'eta': self.eta,
-        }
-        self._init_neurons(neuron_count, **kwargs)
+        self.samples_train = samples_train
+        self.samples_test = samples_test
+        self.labels_train = labels_train
+        self.labels_test = labels_test
+        self.evolutions = []
+        # Using the bias method.
+        """
+        self.weights = [
+                        randint(1, 1000) / 5000
+                        for _ in range(0, len(self.samples_train[0])+1) ]
+        self.weights[0] *= -1
+        """
+        rgen = numpy.random.RandomState(1)
+        self.weights = rgen.normal(
+            loc=0.0,
+            scale=0.01,
+            size=1+samples_train.shape[1])
 
     def train_model(self):
         """
         Fits the model.
         """
-        # Get features and actuals.
-        X = [ # method returning numpy
-            numpy.array(f[0: 4]) # hardcoded
-            for f in self.splitter.get_training_samples_array() ]
-        y = [
-            y[-1] # hardcoded
-            for y in self.splitter.get_training_samples_array() ]
-
-        neuron = self.neurons[0] # weights should be in the neuron
         w = self.weights
-        t = self.threshold
+        X = self.samples_train
+        Y = self.labels_train
 
         for epoch in range(0, self.epochs):
+            neuron = Neuron()
             errors = 0
             for i in range(0, len(X)):
                 # Calculate decision; delta.
                 delta = self.eta * (
-                    y[i] - neuron.decide_bias(w=w[1:], x=X[i], t=w[0]))
-                
-                # A pply delta to weights and update bias.
-                w[1:] = w[1:] + delta * X[i]
+                    Y[i][-1] - neuron.decide_bias(w=w, x=X[i]))
+
+                # Apply delta to weights and update bias.
+                w[1:] += delta * X[i]
                 w[0] += delta
 
-                # Tally erroneous decisions.
-                errors += int(delta) != 0
-            self.solutions.append({
-                                'epoch': epoch,
-                                'errors': errors, 
-                                'error rate': errors/len(X),
-                                'weights': w,
-                                'function': f'f(x*T({w})'})
+                # Tally incorrect predictions.
+                errors += int(delta != 0.0)
+
+            # Set report and store neuron.
+            neuron.set_report([epoch, errors/len(X), w])
+            self.evolutions.append(neuron)
+
             if errors == 0:
                 break
 
     def test_model(
             self,
-            key: str) -> (numpy.array, numpy.array):
+            neuron: Neuron,) -> (list, list, list):
         """
-        Tests the most accurate function against the testing samples
-        and returns the results.
+        Tests a function against the testing samples and returns
+        results. \n
+        Returns:
+            numpy.array
         """
-        neuron = self.neurons[0]
-        w = self.solutions[self.get_solution_index('error rate')].get('weights')
-
-        self.splitter.set_field(key, numpy.nan)
-
-        X = [
-            numpy.array(f[0: 4]) # hardcoded
-            for f in self.splitter.get_testing_samples_array() ]
-        y = [
-            y[-3:] # hardcoded
-            for y in self.splitter.get_testing_samples_array() ]
-
+        w = neuron.report[-1]
+        X = self.samples_test
+        Y = self.labels_test
+        txt_label = []
+        int_label = []
+        pre_label = []
         for i in range(0, len(X)):
-            y[i][2] = neuron.decide_bias(w=w[1:], x=X[i], t=w[0])
+            txt_label.append(Y[i][0])
+            int_label.append(Y[i][1])
+            pre_label.append(neuron.decide_bias(w=w, x=X[i]))
 
-        return (w, y)
+        return (txt_label, int_label, pre_label)
 
-
-
-    def _init_weights(
-            self,
-            n: int,
-            bias: bool,):
+    def get_evolutions(self) -> int:
         """
-        Initialize n random weights plus a bias weight if specified.
-        Store as numpy.array in Perceptron.
+        Return all evolutions of the neuron.
         """
-        #features = self.splitter.get_feature_count() + bias - feat_count   # review this
-        #samples = len(self.splitter.get_training_samples()) # remove this
-
-        self.weights = [
-                        random()
-                        for _ in range(0, bias+n) ]
-
-        self.weights[0] *= -1
-
-        self.weights = numpy.array(self.weights)
-
-    def _init_neurons(
-            self,
-            n: int,
-            **kwargs,):
-        """
-        Initialize n Neurons.
-        """
-        self.neurons = [
-                        Neuron(**kwargs)
-                        for _ in range(0, n) ]
-
-    def get_solution_index(
-            self,
-            key: str) -> int:
-        """
-        Iterate over the self.solutions and return the solution index
-        with the lowest error rate.
-        """
-        max_ = float("-inf")
-        index = None
-        for i, solution in enumerate(self.solutions):
-            if solution.get(key) > max_:
-                index = i
-        return index
+        return self.evolutions
